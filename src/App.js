24d4040f19992aeba19e2a5e0d6a041d28920502
dayzgirl <69809989+dayzgirl@users.noreply.github.com>
2024-04-20 17:24:38 -0400

    Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Navbar } from 'react-bootstrap';
 import './App.css';
+import singlePost from './components/Single-post/single-post';
+import Posts from './components/Posts/posts';
+import Home from './components/Home/home';
+import Contact from './components/Contact/contact';
 
 function App() {
-  return <div className='App'>App</div>
+  return (
+    <div className='App'>App</div>
+      <Navbar />
+      <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={Posts} path="/posts" exact />
+          <Route component={singlePost} path="/posts/:postId" />
+          <Route component={Contact} path="/contact" />
+      </Switch>
+      <Footer/>
+  )
 }
 
 export default App;
---
+++ src/components/Single-post/single-post.js
@@ -0,0 +1,7 @@
+import "./single-post.css"
+
+const Single-Post = {} => {
+    return <div>Single Post</div>;
+};
+
+export default Single-Post;
